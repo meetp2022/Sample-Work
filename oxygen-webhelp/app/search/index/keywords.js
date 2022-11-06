fba639ed9858023ede466b53a952fd7b33ad0bbd
MEET PATEL <101440127+meetp2022@users.noreply.github.com>
2022-11-06 13:10:23 +0530

    first commit

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,26 @@
+var keywords=[{w:"The",p:["p0","p3","p4","p9","p13","p18"]},{w:"basics",p:["p0"]},{w:"Context",p:["p1"]},{w:"Sensitive",p:["p1"]},{w:"Help",p:["p1"]},{w:"Composing",p:["p2"]},{w:"and",p:["p2","p5","p11","p16"]},{w:"sending",p:["p2"]},{w:"email",p:["p2","p14"]},{w:"messages",p:["p2","p14"]},{w:"Inserting",p:["p3","p4"]},{w:"battery",p:["p3","p9"]},{w:"SIM",p:["p4"]},{w:"card",p:["p4"]},{w:"Locking",p:["p5"]},{w:"unlocking",p:["p5"]},{w:"your",p:["p5"]},{w:"phone",p:["p5","p18"]},{w:"Making",p:["p6"]},{w:"a",p:["p6","p7"]},{w:"call",p:["p6","p7"]},{w:"Setting",p:["p7","p10"]},{w:"up",p:["p7","p10"]},{w:"conference",p:["p7"]},{w:"Receiving",p:["p8"]},{w:"files",p:["p8"]},{w:"using",p:["p8"]},{w:"Bluetooth\u00AE",p:["p8","p11"]},{w:"Removing",p:["p9"]},{w:"an",p:["p10"]},{w:"e-mail",p:["p10","p15"]},{w:"address",p:["p10"]},{w:"Turning",p:["p11"]},{w:"on",p:["p11"]},{w:"off",p:["p11"]},{w:"Brief",p:["p12"]},{w:"specifications",p:["p12"]},{w:"Adjusting",p:["p13"]},{w:"volume",p:["p13"]},{w:"Deleting",p:["p14"]},{w:"one",p:["p14"]},{w:"or",p:["p14"]},{w:"more",p:["p14"]},{w:"Important",p:["p16"]},{w:"keys",p:["p16"]},{w:"shortcuts",p:["p16"]},{w:"Introduction",p:["p17"]},{w:"Repairing",p:["p18"]},{w:"Running",p:["p19"]},{w:"problem",p:["p19"]},{w:"diagnostic",p:["p19"]},{w:"report",p:["p19"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2, 3, 4];
+ph["p2"]=[5, 6, 7, 8, 9];
+ph["p3"]=[10, 0, 11];
+ph["p4"]=[10, 0, 12, 13];
+ph["p5"]=[14, 6, 15, 16, 17];
+ph["p6"]=[18, 19, 20];
+ph["p7"]=[21, 22, 19, 23, 20];
+ph["p8"]=[24, 25, 26, 27];
+ph["p9"]=[28, 0, 11];
+ph["p10"]=[21, 22, 29, 30, 31];
+ph["p12"]=[35, 36];
+ph["p11"]=[32, 27, 33, 6, 34];
+ph["p14"]=[39, 40, 41, 42, 8, 9];
+ph["p13"]=[37, 0, 38];
+ph["p16"]=[43, 44, 6, 45];
+ph["p15"]=[30];
+ph["p18"]=[47, 0, 17];
+ph["p17"]=[46];
+ph["p19"]=[48, 49, 50, 51];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {34:-1,33:34,35:1,21:34,36:1,31:1,1:-1,29:34};
